92dd52201be7b319f472da7cfbc369236ae4560c
andieguo <andieguo@foxmail.com>
2015-12-26 14:58:05 +0800

    fix detailServlet

--- src/main/java/com/education/experiment/cloudstorage/DetailFileServlet.java
+++ src/main/java/com/education/experiment/cloudstorage/DetailFileServlet.java
@@ -47,42 +47,48 @@ public class DetailFileServlet extends HttpServlet {
 			// 获取用户提交的文件名称
 			String uuidname = new String(request.getParameter("filename").getBytes("ISO-8859-1"), "UTF-8");
 			System.out.println("uuidname:" + uuidname);
-			File temp = new File(System.getProperty("user.home") + File.separator + "temp");
-			if (!temp.exists()) temp.mkdir();
-			File f = new File(temp.getPath() + File.separator + uuidname);// 创建临时文件，读取HDFS上的文件存储在本地临时文件中，再文件f的内容返回给response
-			String dst = "/tomcat/users/" + ub.getUserId() + "/"+command+ "/" + uuidname;
-			// 开始从HDFS上读取文件
-			FileSystem fs = FileSystem.get(conf);
-			InputStream hadopin = null;
-			OutputStream bos = new BufferedOutputStream(new FileOutputStream(f));
-			System.out.println("dst:" + dst);
-			Path hdfsPath = new Path(dst);
-			if (!fs.exists(hdfsPath)) {// 服务器端的内容不存在
-				request.getRequestDispatcher("/error.jsp?result=下载资源不存在!").forward(request, response);
-			} else {
-				try {
-					hadopin = fs.open(hdfsPath);
-					IOUtils.copyBytes(hadopin, bos, 4096, true);
-					// 读取文件结束,将文件f的内容返回给response
-					if (f.exists()) {
-						// 创建一 个输入流对象和指定的文件相关联
-						FileInputStream input = new FileInputStream(f);
-						if(command.equals("notes")){
-							NoteBean noteBean = HtmlParseUtil.parse(input);
-							request.setAttribute("noteBean", noteBean);
-							request.getRequestDispatcher("/detailnote.jsp").forward(request, response);
-						}else{
-							String content = FileUtil.readInputStream(input);
-							request.setAttribute("content", content);
-							request.getRequestDispatcher("/detailfile.jsp").forward(request, response);
+			if (uuidname.endsWith("txt") || uuidname.endsWith("sql") || uuidname.endsWith("xml") || uuidname.endsWith("properties")) {
+				File temp = new File(System.getProperty("user.home") + File.separator + "temp");
+				if (!temp.exists())
+					temp.mkdir();
+				File f = new File(temp.getPath() + File.separator + uuidname);// 创建临时文件，读取HDFS上的文件存储在本地临时文件中，再文件f的内容返回给response
+				String dst = "/tomcat/users/" + ub.getUserId() + "/" + command + "/" + uuidname;
+				// 开始从HDFS上读取文件
+				FileSystem fs = FileSystem.get(conf);
+				InputStream hadopin = null;
+				OutputStream bos = new BufferedOutputStream(new FileOutputStream(f));
+				System.out.println("dst:" + dst);
+				Path hdfsPath = new Path(dst);
+				if (!fs.exists(hdfsPath)) {// 服务器端的内容不存在
+					request.getRequestDispatcher("/error.jsp?result=下载资源不存在!").forward(request, response);
+				} else {
+					try {
+						hadopin = fs.open(hdfsPath);
+						IOUtils.copyBytes(hadopin, bos, 4096, true);
+						// 读取文件结束,将文件f的内容返回给response
+						if (f.exists()) {
+							// 创建一 个输入流对象和指定的文件相关联
+							FileInputStream input = new FileInputStream(f);
+							if (command.equals("notes")) {
+								NoteBean noteBean = HtmlParseUtil.parse(input);
+								request.setAttribute("noteBean", noteBean);
+								request.getRequestDispatcher("/detailnote.jsp").forward(request, response);
+							} else {
+								String content = FileUtil.readInputStream(input);
+								request.setAttribute("content", content);
+								request.getRequestDispatcher("/detailfile.jsp").forward(request, response);
+
+							}
+						} else {
+							request.getRequestDispatcher("/error.jsp?result=下载资源不存在!").forward(request, response);
 						}
-					} else {
-						request.getRequestDispatcher("/error.jsp?result=下载资源不存在!").forward(request, response);
+					} finally {
+						IOUtils.closeStream(hadopin);
+						bos.close();
 					}
-				} finally {
-					IOUtils.closeStream(hadopin);
-					bos.close();
 				}
+			} else {
+				request.getRequestDispatcher("/error.jsp?result=不支持预览的文件格式!").forward(request, response);
 			}
 		}
 	}
